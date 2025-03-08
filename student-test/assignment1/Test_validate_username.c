4bd4b8acda33db20e075f0793b91512974fbe64c
victor <victor@Ubuntu.myguest.virtualbox.org>
2025-03-09 00:55:02 +0800

    tired

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -14,9 +14,17 @@
 */
 void test_validate_my_username()
 {
-    /**
-     * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
-     * config file and my_username() functions are setup properly
-     */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    // Get the hardcoded username
+    const char *hardcoded_username = my_username();
+
+    // Get the username from the config file (dynamically allocated)
+    char *config_username = malloc_username_from_conf_file();
+
+    // Validate that both usernames match
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(hardcoded_username, config_username, 
+                                     "The username from config file does not match the hardcoded username.");
+
+    // Free dynamically allocated memory
+    free(config_username);
 }
+
